6efa29f5b5b7a65b8eb4a6ceff247a0cfefa3062
Alex Bukengolz <alexb@q4websystems.com>
2015-12-15 19:51:32 -0500

    fixed bubble chart on splash screen

--- _assets/javascripts/splash.js
+++ _assets/javascripts/splash.js
@@ -62,35 +62,140 @@ if(window && window.outerWidth && window.outerWidth > 700) {
     var viz = Vizabi('BubbleChart', document.getElementById('embeddable-container'), {
         state: {
             time: {
-                value: '2010'
+                dim: 'time',
+                value: '1950',
+                start: '1800',
+                end: '2015',
+                playable: true,
+                playing: true,
+                loop: false,
+                round: 'round',
+                delay: 250,
+                delayAnimations: 250,
+                delayStart: 1200,
+                delayEnd: 75,
+                delaySet: false,
+                unit: 'year',
+                step: 1,
+                adaptMinMaxZoom: false,
+                formatInput: '%Y',
+                formatOutput: '%Y',
+                xLogStops: [],
+                yMaxMethod: 'latest',
+                record: false,
+                probeX: 0,
+                tailFatX: 1,
+                tailCutX: 0,
+                tailFade: 1,
+                xScaleFactor: 1,
+                xScaleShift: 0,
+                xPoints: 50,
+                trails: true,
+                lockNonSelected: 0
+            },
+            entities: {
+                show: {
+                    geo: [
+                        '*'
+                    ],
+                    'geo.cat': [
+                        'country'
+                    ]
+                },
+                select: [],
+                highlight: [],
+                opacitySelectDim: 0.3,
+                opacityRegular: 1,
+                needUpdate: {},
+                dim: 'geo'
+            },
+            marker: {
+                space: [
+                    'entities',
+                    'time'
+                ],
+                type: 'geometry',
+                shape: 'circle',
+                label: {
+                    use: 'property',
+                    which: 'geo.name'
+                },
+                axis_y: {
+                    use: 'indicator',
+                    which: 'u5mr',
+                    min: null,
+                    max: null,
+                    fakeMin: null,
+                    fakeMax: null,
+                    scaleType: 'linear',
+                    allow: {
+                        scales: [
+                            'linear',
+                            'log'
+                        ]
+                    }
+                },
+                axis_x: {
+                    use: 'indicator',
+                    which: 'gdp_pc',
+                    min: null,
+                    max: null,
+                    fakeMin: null,
+                    fakeMax: null,
+                    scaleType: 'log',
+                    allow: {
+                        scales: [
+                            'linear',
+                            'log'
+                        ]
+                    }
+                },
+                color: {
+                    use: 'property',
+                    palette: {
+                        asi: '#FF5872',
+                        eur: '#FFE700',
+                        ame: '#7FEB00',
+                        afr: '#00D5E9',
+                        _default: '#ffb600'
+                    },
+                    which: 'geo.name',
+                    scaleType: 'ordinal',
+                    allow: {
+                        names: [
+                            '!geo.name'
+                        ]
+                    }
+                },
+                size: {
+                    use: 'indicator',
+                    min: 0.04,
+                    max: 0.9,
+                    which: 'pop',
+                    scaleType: 'linear',
+                    allow: {
+                        scales: [
+                            'linear',
+                            'log'
+                        ]
+                    }
+                }
             }
         },
         data: {
             reader: 'csv',
-            path: '/preview/data/waffles/basic-indicators.csv'
-            //path: 'https://dl.dropboxusercontent.com/u/4933279/csv/basic-indicators.csv'
-        },
-        bind: {
-            ready: function() {
-                viz.setOptions({
-                    state: {
-                        time: {
-                            playing: true
-                        }
-                    }
-                });
-            }
+            path: '/preview/data/waffles/dont-panic-poverty.csv'
         }
     });
 
     var embeddable = document.getElementById('embeddable-container');
     var close_btn = document.getElementById('close-embeddable');
+    
     embeddable.addEventListener('click', function(e) {
         addClass(document.body, 'is-product-tour');
     });
 
     close_btn.addEventListener('click', function(e) {
-        console.log('test');
         removeClass(document.body, 'is-product-tour');
         e.preventDefault();
     });
